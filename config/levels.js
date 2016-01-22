e58c56906a2eeadf54eb22d1c0775c56a824ddc7
Goutham Veeramachaneni <cs14btech11014@iith.ac.in>
2016-01-22 11:22:53 +0530

    Redid the levels format for flexibility
    Signed-off-by: Goutham Veeramachaneni <cs14btech11014@iith.ac.in>

--- config/levels.js
+++ config/levels.js
@@ -1,100 +1,168 @@
-var levels = {
-	1: {
+// var levels = {
+// 	1: {
+// 		answer: "nvision"
+// 	},
+// 	2: {
+// 		answer: "vlc"	//NEEDS HINT
+// 	},
+// 	3: {
+// 		answer: "firefox" // NEEDS HINT
+// 	},
+// 	4: {
+// 		answer: "vespa"
+// 	},
+// 	5: {
+// 		answer: "pink floyd" // NEEDS HINT
+// 	},
+// 	6: {
+// 		answer: "pink floyd" // NEEDS A Q
+// 	},
+// 	7: {
+// 		answer: "anagram"
+// 	},
+// 	8: {
+// 		answer: "emma watson" // NEEDS IMPROV
+// 	},
+// 	9: {
+// 		answer: "bean" // BEAN or MR.Bean?
+// 	},
+// 	10: {
+// 		answer: "jane ubrein" //Explain once again?
+// 	},
+// 	11: {
+// 		answer: "totem"
+// 	},
+// 	12: {
+// 		answer: "advaya"
+// 	},
+// 	13: {
+// 		answer: "final destination"
+// 	},
+// 	14: {
+// 		answer: "gangotri"
+// 	},
+// 	15: {
+// 		answer: "wilkie"
+// 	},
+// 	16: {
+// 		answer: "48988659276962496"
+// 	},
+// 	17: {
+// 		answer: "rugby"
+// 	},
+// 	18: {
+// 		answer: "dagon"
+// 	},
+// 	19: {
+// 		answer: "neil degrasse tyson"
+// 	},
+// 	20: {
+// 		answer: "iupac"
+// 	},
+// 	21: {
+// 		answer: "tvf pitchers"
+// 	},
+// 	22: {
+// 		answer: "thomas young"
+// 	},
+// 	23: {
+// 		answer: "bryan adams"
+// 	},
+// 	24: {
+// 		answer: "japan"
+// 	},
+// 	25: {
+// 		answer: "eminem"
+// 	},
+// 	26: {
+// 		answer: "enigma"
+// 	},
+// 	27: {
+// 		answer: "imran khan"
+// 	},
+// 	28: {
+// 		answer: "clyde"
+// 	},
+// 	29: {
+// 		answer: "serious"
+// 	},
+// 	30: {
+// 		answer: "richard brooke"
+// 	},
+// 	31: {
+// 		answer: "chandra x laboratory"
+// 	},
+// 	32: {
+// 		answer: "chandra x laboratory"
+// 	}
+// };
+
+levels = [{
+					// EMPTY COZ we start from 1
+	},{
 		answer: "nvision"
-	},
-	2: {
+	},{
 		answer: "vlc"	//NEEDS HINT
-	},
-	3: {
+	},{
 		answer: "firefox" // NEEDS HINT
-	},
-	4: {
-		answer: "vespa"
-	},
-	5: {
-		answer: "pink floyd" // NEEDS HINT
-	},
-	6: {
-		answer: "pink floyd" // NEEDS A Q
-	},
-	7: {
-		answer: "anagram"
-	},
-	8: {
+	},{
 		answer: "emma watson" // NEEDS IMPROV
-	},
-	9: {
+	},{
+		answer: "vespa"
+	},{
+		answer: "pink floyd" // NEEDS HINT
+	},{
+		answer: "pink floyd" // NEEDS A Q
+	},{
+		answer: "anagram"
+	},{
 		answer: "bean" // BEAN or MR.Bean?
-	},
-	10: {
+	},{
 		answer: "jane ubrein" //Explain once again?
-	},
-	11: {
+	},{
 		answer: "totem"
-	},
-	12: {
+	},{
 		answer: "advaya"
-	},
-	13: {
+	},{
 		answer: "final destination"
-	},
-	14: {
+	},{
 		answer: "gangotri"
-	},
-	15: {
+	},{
 		answer: "wilkie"
-	},
-	16: {
+	},{
 		answer: "48988659276962496"
-	},
-	17: {
+	},{
 		answer: "rugby"
-	},
-	18: {
+	},{
 		answer: "dagon"
-	},
-	19: {
+	},{
 		answer: "neil degrasse tyson"
-	},
-	20: {
+	},{
 		answer: "iupac"
-	},
-	21: {
+	},{
 		answer: "tvf pitchers"
-	},
-	22: {
+	},{
 		answer: "thomas young"
-	},
-	23: {
+	},{
 		answer: "bryan adams"
-	},
-	24: {
+	},{
 		answer: "japan"
-	},
-	25: {
+	},{
 		answer: "eminem"
-	},
-	26: {
+	},{
 		answer: "enigma"
-	},
-	27: {
-		answer: "jaane tu... ya jaane na"
-	},
-	28: {
+	},{
+		answer: "imran khan"
+	},{
 		answer: "clyde"
-	},
-	29: {
+	},{
 		answer: "serious"
-	},
-	30: {
+	},{
 		answer: "richard brooke"
-	},
-	31: {
+	},{
 		answer: "chandra x laboratory"
-	},
-	32: {
+	},{
 		answer: "chandra x laboratory"
-	}
-}
+	}];
 
 module.exports = levels;
