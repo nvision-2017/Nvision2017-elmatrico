877fd8919e0e2a89f5d309e6bb7d1e3d81fa1469
Goutham Veeramachaneni <cs14btech11014@iith.ac.in>
2016-01-22 13:51:52 +0530

    Added Alex Karpovsky
    Signed-off-by: Goutham Veeramachaneni <cs14btech11014@iith.ac.in>

--- config/levels.js
+++ config/levels.js
@@ -1,102 +1,3 @@
-// var levels = {
-// 	1: {
-// 		answer: "nvision"
-// 	},
-// 	2: {
-// 		answer: "vlc"	//NEEDS HINT
-// 	},
-// 	3: {
-// 		answer: "firefox" // NEEDS HINT
-// 	},
-// 	4: {
-// 		answer: "vespa"
-// 	},
-// 	5: {
-// 		answer: "pink floyd" // NEEDS HINT
-// 	},
-// 	6: {
-// 		answer: "pink floyd" // NEEDS A Q
-// 	},
-// 	7: {
-// 		answer: "anagram"
-// 	},
-// 	8: {
-// 		answer: "emma watson" // NEEDS IMPROV
-// 	},
-// 	9: {
-// 		answer: "bean" // BEAN or MR.Bean?
-// 	},
-// 	10: {
-// 		answer: "jane ubrein" //Explain once again?
-// 	},
-// 	11: {
-// 		answer: "totem"
-// 	},
-// 	12: {
-// 		answer: "advaya"
-// 	},
-// 	13: {
-// 		answer: "final destination"
-// 	},
-// 	14: {
-// 		answer: "gangotri"
-// 	},
-// 	15: {
-// 		answer: "wilkie"
-// 	},
-// 	16: {
-// 		answer: "48988659276962496"
-// 	},
-// 	17: {
-// 		answer: "rugby"
-// 	},
-// 	18: {
-// 		answer: "dagon"
-// 	},
-// 	19: {
-// 		answer: "neil degrasse tyson"
-// 	},
-// 	20: {
-// 		answer: "iupac"
-// 	},
-// 	21: {
-// 		answer: "tvf pitchers"
-// 	},
-// 	22: {
-// 		answer: "thomas young"
-// 	},
-// 	23: {
-// 		answer: "bryan adams"
-// 	},
-// 	24: {
-// 		answer: "japan"
-// 	},
-// 	25: {
-// 		answer: "eminem"
-// 	},
-// 	26: {
-// 		answer: "enigma"
-// 	},
-// 	27: {
-// 		answer: "imran khan"
-// 	},
-// 	28: {
-// 		answer: "clyde"
-// 	},
-// 	29: {
-// 		answer: "serious"
-// 	},
-// 	30: {
-// 		answer: "richard brooke"
-// 	},
-// 	31: {
-// 		answer: "chandra x laboratory"
-// 	},
-// 	32: {
-// 		answer: "chandra x laboratory"
-// 	}
-// };
-
 levels = [{
 					// EMPTY COZ we start from 1
 	},{
@@ -124,8 +25,8 @@ levels = [{
 		answer: "pink floyd", // NEEDS HINT
 		file: "pink1"
 	},{
-		answer: "pink floyd", // NEEDS A Q
-		file: "pink2"
+		answer: "alex karpovsky", // NEEDS HINT
+		file: "alex"
 	},{
 		answer: "anagram",
 		file: "anagram"
